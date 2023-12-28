ce67cdfdc9cca4b67bd942f8a5b51aa33581c861
Ali <a95blnd@gmail.com>
2023-12-28 12:18:07 +0330

    create scraper for ids

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 from network.api.instrument_data import update_contract_info
+from services.instrument_data import extract_instrument_ids
 
 
-update_contract_info("IRO9AGAH0401")
+
+list_ids = extract_instrument_ids()
+for index, id in enumerate(list_ids):
+    update_contract_info(id)
+    print(index, id)
